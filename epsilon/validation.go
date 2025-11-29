e5c8c3ad2b88484dcbacc6d58799ec69863d90b7
Andrea Pivetta <vanpivix@gmail.com>
2025-11-29 14:46:52 +0100

    Everything that is valid in any unti and spec test is marked as valid

--- epsilon/validation.go
+++ epsilon/validation.go
@@ -198,73 +198,21 @@ func (v *validator) validate(instruction Instruction) error {
 		return v.validateConst(F32)
 	case F64Const:
 		return v.validateConst(F64)
-	case I32Eq,
-		I32Ne,
-		I32LtS,
-		I32LtU,
-		I32GtS,
-		I32GtU,
-		I32LeS,
-		I32LeU,
-		I32GeS,
+	case I32Eq, I32Ne, I32LtS, I32LtU, I32GtS, I32GtU, I32LeS, I32LeU, I32GeS,
 		I32GeU:
 		return v.validateBinaryOp(I32, I32)
-	case I64Eq,
-		I64Ne,
-		I64LtS,
-		I64LtU,
-		I64GtS,
-		I64GtU,
-		I64LeS,
-		I64LeU,
-		I64GeS,
+	case I64Eq, I64Ne, I64LtS, I64LtU, I64GtS, I64GtU, I64LeS, I64LeU, I64GeS,
 		I64GeU:
 		return v.validateBinaryOp(I64, I32)
-	case F32Eq,
-		F32Ne,
-		F32Lt,
-		F32Gt,
-		F32Le,
-		F32Ge:
+	case F32Eq, F32Ne, F32Lt, F32Gt, F32Le, F32Ge:
 		return v.validateBinaryOp(F32, I32)
-	case F64Eq,
-		F64Ne,
-		F64Lt,
-		F64Gt,
-		F64Le,
-		F64Ge:
+	case F64Eq, F64Ne, F64Lt, F64Gt, F64Le, F64Ge:
 		return v.validateBinaryOp(F64, I32)
-	case I32Add,
-		I32Sub,
-		I32Mul,
-		I32DivS,
-		I32DivU,
-		I32RemS,
-		I32RemU,
-		I32And,
-		I32Or,
-		I32Xor,
-		I32Shl,
-		I32ShrS,
-		I32ShrU,
-		I32Rotl,
-		I32Rotr:
+	case I32Add, I32Sub, I32Mul, I32DivS, I32DivU, I32RemS, I32RemU, I32And,
+		I32Or, I32Xor, I32Shl, I32ShrS, I32ShrU, I32Rotl, I32Rotr:
 		return v.validateBinaryOp(I32, I32)
-	case I64Add,
-		I64Sub,
-		I64Mul,
-		I64DivS,
-		I64DivU,
-		I64RemS,
-		I64RemU,
-		I64And,
-		I64Or,
-		I64Xor,
-		I64Shl,
-		I64ShrS,
-		I64ShrU,
-		I64Rotl,
-		I64Rotr:
+	case I64Add, I64Sub, I64Mul, I64DivS, I64DivU, I64RemS, I64RemU, I64And,
+		I64Or, I64Xor, I64Shl, I64ShrS, I64ShrU, I64Rotl, I64Rotr:
 		return v.validateBinaryOp(I64, I64)
 	case F32Add, F32Sub, F32Mul, F32Div, F32Min, F32Max, F32Copysign:
 		return v.validateBinaryOp(F32, F32)
@@ -280,10 +228,7 @@ func (v *validator) validate(instruction Instruction) error {
 		return v.validateUnaryOp(F32, F32)
 	case F64Abs, F64Neg, F64Ceil, F64Floor, F64Trunc, F64Nearest, F64Sqrt:
 		return v.validateUnaryOp(F64, F64)
-	case I32TruncF32S,
-		I32TruncF32U,
-		I32ReinterpretF32,
-		I32TruncSatF32S,
+	case I32TruncF32S, I32TruncF32U, I32ReinterpretF32, I32TruncSatF32S,
 		I32TruncSatF32U:
 		return v.validateUnaryOp(F32, I32)
 	case I32TruncF64S, I32TruncF64U, I32TruncSatF64S, I32TruncSatF64U:
@@ -372,6 +317,19 @@ func (v *validator) validate(instruction Instruction) error {
 		return v.validateStore(instruction, F32)
 	case F64Store:
 		return v.validateStore(instruction, F64)
+	case V128Load:
+		return v.validateLoad(instruction, V128)
+	case V128Load8x8S, V128Load8x8U, V128Load16x4S, V128Load16x4U, V128Load32x2S,
+		V128Load32x2U, V128Load64Splat, V128Load64Zero:
+		return v.validateLoadN(instruction, V128, 8)
+	case V128Load8Splat:
+		return v.validateLoadN(instruction, V128, 1)
+	case V128Load16Splat:
+		return v.validateLoadN(instruction, V128, 2)
+	case V128Load32Splat, V128Load32Zero:
+		return v.validateLoadN(instruction, V128, 4)
+	case V128Store:
+		return v.validateStore(instruction, V128)
 	case V128Const:
 		return v.validateConst(V128)
 	case V128Load8Lane:
@@ -390,11 +348,8 @@ func (v *validator) validate(instruction Instruction) error {
 		return v.validateSimdStoreLane(instruction, 4)
 	case V128Store64Lane:
 		return v.validateSimdStoreLane(instruction, 8)
-	case I8x16ExtractLaneS,
-		I8x16ExtractLaneU,
-		I16x8ExtractLaneS,
-		I16x8ExtractLaneU,
-		I32x4ExtractLane:
+	case I8x16ExtractLaneS, I8x16ExtractLaneU, I16x8ExtractLaneS,
+		I16x8ExtractLaneU, I32x4ExtractLane:
 		return v.validateUnaryOp(V128, I32)
 	case I64x2ExtractLane:
 		return v.validateUnaryOp(V128, I64)
@@ -402,6 +357,71 @@ func (v *validator) validate(instruction Instruction) error {
 		return v.validateUnaryOp(V128, F32)
 	case F64x2ExtractLane:
 		return v.validateUnaryOp(V128, F64)
+	case V128AnyTrue, I8x16AllTrue, I8x16Bitmask, I16x8AllTrue, I16x8Bitmask,
+		I32x4AllTrue, I32x4Bitmask, I64x2AllTrue, I64x2Bitmask:
+		return v.validateUnaryOp(V128, I32)
+	case V128Not:
+		return v.validateUnaryOp(V128, V128)
+	case V128And, V128Andnot, V128Or, V128Xor:
+		return v.validateBinaryOp(V128, V128)
+	case V128Bitselect:
+		return v.validateBitselect()
+	case I8x16Splat, I16x8Splat, I32x4Splat:
+		return v.validateUnaryOp(I32, V128)
+	case I64x2Splat:
+		return v.validateUnaryOp(I64, V128)
+	case F32x4Splat:
+		return v.validateUnaryOp(F32, V128)
+	case F64x2Splat:
+		return v.validateUnaryOp(F64, V128)
+	case I8x16ReplaceLane, I16x8ReplaceLane, I32x4ReplaceLane:
+		return v.validateReplaceLane(I32)
+	case I64x2ReplaceLane:
+		return v.validateReplaceLane(I64)
+	case F32x4ReplaceLane:
+		return v.validateReplaceLane(F32)
+	case F64x2ReplaceLane:
+		return v.validateReplaceLane(F64)
+	case I8x16Shl, I8x16ShrS, I8x16ShrU, I16x8Shl, I16x8ShrS, I16x8ShrU,
+		I32x4Shl, I32x4ShrS, I32x4ShrU, I64x2Shl, I64x2ShrS, I64x2ShrU:
+		return v.validateSimdShift()
+	case I8x16Shuffle, I8x16Swizzle:
+		return v.validateBinaryOp(V128, V128)
+	case I8x16Eq, I8x16Ne, I8x16LtS, I8x16LtU, I8x16GtS, I8x16GtU, I8x16LeS,
+		I8x16LeU, I8x16GeS, I8x16GeU, I16x8Eq, I16x8Ne, I16x8LtS, I16x8LtU,
+		I16x8GtS, I16x8GtU, I16x8LeS, I16x8LeU, I16x8GeS, I16x8GeU, I32x4Eq,
+		I32x4Ne, I32x4LtS, I32x4LtU, I32x4GtS, I32x4GtU, I32x4LeS, I32x4LeU,
+		I32x4GeS, I32x4GeU, I64x2Eq, I64x2Ne, I64x2LtS, I64x2GtS, I64x2LeS,
+		I64x2GeS, F32x4Eq, F32x4Ne, F32x4Lt, F32x4Gt, F32x4Le, F32x4Ge, F64x2Eq,
+		F64x2Ne, F64x2Lt, F64x2Gt, F64x2Le, F64x2Ge, I8x16Add, I8x16AddSatS,
+		I8x16AddSatU, I8x16Sub, I8x16SubSatS, I8x16SubSatU, I8x16MinS, I8x16MinU,
+		I8x16MaxS, I8x16MaxU, I8x16AvgrU, I16x8Add, I16x8AddSatS, I16x8AddSatU,
+		I16x8Sub, I16x8SubSatS, I16x8SubSatU, I16x8Mul, I16x8MinS, I16x8MinU,
+		I16x8MaxS, I16x8MaxU, I16x8AvgrU, I16x8Q15mulrSatS, I32x4Add, I32x4Sub,
+		I32x4Mul, I32x4MinS, I32x4MinU, I32x4MaxS, I32x4MaxU, I32x4DotI16x8S,
+		I64x2Add, I64x2Sub, I64x2Mul, F32x4Add, F32x4Sub, F32x4Mul, F32x4Div,
+		F32x4Min, F32x4Max, F32x4Pmin, F32x4Pmax, F64x2Add, F64x2Sub, F64x2Mul,
+		F64x2Div, F64x2Min, F64x2Max, F64x2Pmin, F64x2Pmax, I8x16NarrowI16x8S,
+		I8x16NarrowI16x8U, I16x8NarrowI32x4S, I16x8NarrowI32x4U,
+		I16x8ExtmulLowI8x16S, I16x8ExtmulHighI8x16S, I16x8ExtmulLowI8x16U,
+		I16x8ExtmulHighI8x16U, I32x4ExtmulLowI16x8S, I32x4ExtmulHighI16x8S,
+		I32x4ExtmulLowI16x8U, I32x4ExtmulHighI16x8U, I64x2ExtmulLowI32x4S,
+		I64x2ExtmulHighI32x4S, I64x2ExtmulLowI32x4U, I64x2ExtmulHighI32x4U:
+		return v.validateBinaryOp(V128, V128)
+	case I8x16Abs, I8x16Neg, I8x16Popcnt,
+		I16x8Abs, I16x8Neg, I16x8ExtaddPairwiseI8x16S, I16x8ExtaddPairwiseI8x16U,
+		I32x4Abs, I32x4Neg, I32x4ExtaddPairwiseI16x8S, I32x4ExtaddPairwiseI16x8U,
+		I64x2Abs, I64x2Neg, F32x4Abs, F32x4Neg, F32x4Sqrt, F32x4Ceil, F32x4Floor,
+		F32x4Trunc, F32x4Nearest, F64x2Abs, F64x2Neg, F64x2Sqrt, F64x2Ceil,
+		F64x2Floor, F64x2Trunc, F64x2Nearest, I32x4TruncSatF32x4S,
+		I32x4TruncSatF32x4U, F32x4ConvertI32x4S, F32x4ConvertI32x4U,
+		I16x8ExtendLowI8x16S, I16x8ExtendHighI8x16S, I16x8ExtendLowI8x16U,
+		I16x8ExtendHighI8x16U, I32x4ExtendLowI16x8S, I32x4ExtendHighI16x8S,
+		I32x4ExtendLowI16x8U, I32x4ExtendHighI16x8U, I64x2ExtendLowI32x4S,
+		I64x2ExtendHighI32x4S, I64x2ExtendLowI32x4U, I64x2ExtendHighI32x4U,
+		F64x2PromoteLowF32x4, F32x4DemoteF64x2Zero, I32x4TruncSatF64x2SZero,
+		I32x4TruncSatF64x2UZero, F64x2ConvertLowI32x4S, F64x2ConvertLowI32x4U:
+		return v.validateUnaryOp(V128, V128)
 	case MemoryInit:
 		return v.validateMemoryInit(instruction)
 	case MemoryCopy:
@@ -958,6 +978,42 @@ func (v *validator) validateBinaryOp(inputs ValueType, output ValueType) error {
 		return err
 	}
 	v.pushValue(output)
+	return nil
+}
+
+func (v *validator) validateBitselect() error {
+	if _, err := v.popExpectedValue(V128); err != nil {
+		return err
+	}
+	if _, err := v.popExpectedValue(V128); err != nil {
+		return err
+	}
+	if _, err := v.popExpectedValue(V128); err != nil {
+		return err
+	}
+	v.pushValue(V128)
+	return nil
+}
+
+func (v *validator) validateReplaceLane(scalarType ValueType) error {
+	if _, err := v.popExpectedValue(scalarType); err != nil {
+		return err
+	}
+	if _, err := v.popExpectedValue(V128); err != nil {
+		return err
+	}
+	v.pushValue(V128)
+	return nil
+}
+
+func (v *validator) validateSimdShift() error {
+	if _, err := v.popExpectedValue(I32); err != nil {
+		return err
+	}
+	if _, err := v.popExpectedValue(V128); err != nil {
+		return err
+	}
+	v.pushValue(V128)
 	return nil
 }
 
